c217b26eb02069aae4c028610301f74451fbafab
Rasheed INITS <rasheedinits@Rasheeds-MacBook-Pro.local>
2021-12-27 08:52:00 +0100

    adding headers to prevent cors

--- src/pages/user/user.js
+++ src/pages/user/user.js
@@ -15,7 +15,9 @@ export default {
     created(){
         this.username = this.$route.params.username
   
-        this.axios.get(`https://torre.bio/api/bios/${this.username}`)
+        this.axios.get(`https://torre.bio/api/bios/${this.username}`, {
+                headers: {'Access-Control-Allow-Origin': '*'}
+            })
           .then( (response) => {
               this.person = response.data.person
               this.skills = response.data.strengths
